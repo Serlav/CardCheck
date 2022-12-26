d6a7c8ab3365db3a25a63735f7f94b237dcc8187
Sergey <sergey@localhost>
2022-12-27 02:47:08 +0300

    Alfa Version 0.01

--- app/src/main/java/ru/serlav/cardcheck/MainActivity.kt
+++ app/src/main/java/ru/serlav/cardcheck/MainActivity.kt
@@ -1,33 +1,60 @@
 package ru.serlav.cardcheck
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
-import org.json.JSONObject
 import ru.serlav.cardcheck.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var vm: MainViewModel
 
+   // @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-    }
+        vm = ViewModelProvider(this)[MainViewModel::class.java]
 
-    fun bind(card: CardInfo) {
+//        vm.resultText.observe(this,{
+//            bin.text = it
+//        })
+
         with(binding) {
             search.setOnClickListener {
                 val num = Integer.parseInt(bin.text.toString())
                 cardCheck(num)
-                scheme.text = card.scheme
-                type.text = card.type
-                brand.text = card.brand
+
+                scheme.text = vm.resultText.value?.scheme
+                type.text = vm.resultText.value?.type
+                brand.text = vm.resultText.value?.brand
+                prepaid.text =
+                    (if (vm.resultText.value?.prepaid == true) {
+                        "Yes"
+                    } else {
+                        "No"
+                    }).toString()
+                length.text = vm.resultText.value?.length.toString()
+                luhn.text =
+                    (if (vm.resultText.value?.luhn == true) {
+                        "Yes"
+                    } else {
+                        "No"
+                    }).toString()
+                alpha2.text = vm.resultText.value?.alpha2
+                name.text = vm.resultText.value?.name
+             //   location.text = ("(latitude: ${{ vm.resultText.value?.latitude }}, longitude: ${{ vm.resultText.value?.longitude }}"
+           //             ).toString()
+                bankName.text = vm.resultText.value?.bankName
+                url.text = vm.resultText.value?.url
+                phone.text = vm.resultText.value?.phone
             }
         }
     }
@@ -40,58 +67,13 @@ class MainActivity : AppCompatActivity() {
             url,
             { result ->
                 Log.d("MyLog", "Result: $result")
-                parseCardInfo(result)
+                vm.parseCardInfo(result)
+
             },
             { error ->
                 Log.d("MyLog", "EroR: $error")
             }
         )
         queue.add(request)
-    }
-
-    private fun parseCardInfo(result: String) {
-        val mainObject = JSONObject(result)
-        val item = CardInfo(
-            mainObject.getJSONObject("number").getInt("length"),
-            mainObject.getJSONObject("number").getBoolean("luhn"),
-            mainObject.getString("scheme"),
-            mainObject.getString("type"),
-            mainObject.getString("brand"),
-            mainObject.getBoolean("prepaid"),
-            mainObject.getJSONObject("country").getInt("numeric"),
-            mainObject.getJSONObject("country").getString("alpha2"),
-            mainObject.getJSONObject("country").getString("name"),
-            mainObject.getJSONObject("country").getString("emoji"),
-            mainObject.getJSONObject("country").getString("currency"),
-            mainObject.getJSONObject("country").getInt("latitude"),
-            mainObject.getJSONObject("country").getInt("longitude"),
-
-            mainObject.getJSONObject("bank").getString("name"),
-            mainObject.getJSONObject("bank").getString("url"),
-            mainObject.getJSONObject("bank").getString("phone"),
-            mainObject.getJSONObject("bank").getString("city")
-        )
-
-        Log.d("MyLog", "length: ${item.length}")
-        Log.d("MyLog", "luhn: ${item.luhn}")
-        Log.d("MyLog", "scheme: ${item.scheme}")
-        Log.d("MyLog", "type: ${item.type}")
-        Log.d("MyLog", "brand: ${item.brand}")
-        Log.d("MyLog", "prepaid: ${item.prepaid}")
-        Log.d("MyLog", "numeric: ${item.numeric}")
-        Log.d("MyLog", "alpha2: ${item.alpha2}")
-        Log.d("MyLog", "name: ${item.name}")
-        Log.d("MyLog", "emoji: ${item.emoji}")
-        Log.d("MyLog", "currency: ${item.currency}")
-        Log.d("MyLog", "latitude: ${item.latitude}")
-        Log.d("MyLog", "longitude: ${item.longitude}")
-
-
-        Log.d("MyLog", "name: ${item.name}")
-        Log.d("MyLog", "url: ${item.url}")
-        Log.d("MyLog", "phone: ${item.phone}")
-        Log.d("MyLog", "city: ${item.city}")
-        Log.d("MyLog", "ITEM: $item")
-
     }
 }
---
+++ app/src/main/java/ru/serlav/cardcheck/MainViewModel.kt
@@ -0,0 +1,38 @@
+package ru.serlav.cardcheck
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import org.json.JSONObject
+
+
+class MainViewModel : ViewModel() {
+
+    val resultText = MutableLiveData<CardInfo>()
+
+    fun parseCardInfo(result: String) {
+        val mainObject = JSONObject(result)
+        resultText.value = CardInfo(
+            mainObject.getJSONObject("number").getInt("length"),
+            mainObject.getJSONObject("number").getBoolean("luhn"),
+            mainObject.getString("scheme"),
+            mainObject.getString("type"),
+            mainObject.getString("brand"),
+            mainObject.getBoolean("prepaid"),
+            mainObject.getJSONObject("country").getInt("numeric"),
+            mainObject.getJSONObject("country").getString("alpha2"),
+            mainObject.getJSONObject("country").getString("name"),
+            mainObject.getJSONObject("country").getString("emoji"),
+            mainObject.getJSONObject("country").getString("currency"),
+            mainObject.getJSONObject("country").getInt("latitude"),
+            mainObject.getJSONObject("country").getInt("longitude"),
+
+            mainObject.getJSONObject("bank").getString("name"),
+            mainObject.getJSONObject("bank").getString("url"),
+            mainObject.getJSONObject("bank").getString("phone"),
+            //  mainObject.getJSONObject("bank").getString("city")
+        )
+
+        Log.d("MyLog", "length: ${resultText.value!!.length}")
+    }
+}
